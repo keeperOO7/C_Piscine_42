978b17e4b061f6cbf9468ec129f0874d69a300ff
Keeper is here :) <88516486+keeperOO7@users.noreply.github.com>
2023-07-11 01:09:39 +0100

    Update wdmatch.c

--- Exams-42-Piscine/Level_02/wdmatch/wdmatch.c
+++ Exams-42-Piscine/Level_02/wdmatch/wdmatch.c
@@ -1,54 +1,24 @@
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+void	ft_putstr(char const *str)
 {
-	int i;
+	int		i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
+	while (str[i])
+		write(1, &str[i++], 1);
 }
 
-int		ft_strlen(char *str)
+int main (int ac, char **av)
 {
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-int		main(int ac, char **av)
-{
-	int i;
-	int i2;
-	int wdlen;
-
-	i = 0;
-	i2 = 0;
-	wdlen = 0;
-	if (ac == 3)
-	{
-		while (av[1][i] != '\0')
-		{
-			while (av[2][i2] != '\0')
-			{
-				if (av[1][i] == av[2][i2])
-				{
-					wdlen++;
-					break ;
-				}
-				i2++;
-			}
-			i++;
-		}
-		if (wdlen == ft_strlen(av[1]))
-			ft_putstr(av[1]);
-	}
-	write(1, "\n", 1);
-	return (0);
+	int	i = 0;
+	int	j = 0;
+	char *str1 = av[1];
+	char *str2 =av[2];
+	
+	while (str2[j])
+		if (str2[j++] == str1[i])
+			i += 1;
+	if (!str1[i])
+		ft_putstr(str1);
 }
