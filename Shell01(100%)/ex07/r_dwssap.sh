7d44e4478d49487e78af8091316342e7fe70bdac
Keeper is here :) <88516486+keeperOO7@users.noreply.github.com>
2023-07-11 01:04:11 +0100

    42_Piscine

---
+++ Shell01(100%)/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | sed '/^#/d' | awk 'NR % 2 == 0' | awk -F ":" '{  print $1 }' | rev | sort -fnr | awk -v min="$FT_LINE1" -v max="$FT_LINE2" "NR >= min && NR <= max" | tr '\n' ' '  | sed 's/ /, /g' | sed 's/, $/./g' | tr -d '\n'
